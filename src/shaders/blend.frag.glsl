7c550038ffede6d6cd9c3a5ce3e9ae0e0163c6cd
Vladimir Agafonkin <agafonkin@gmail.com>
2016-11-18 23:17:53 +0200

    more vivid color blending

--- src/shaders/blend.frag.glsl
+++ src/shaders/blend.frag.glsl
@@ -5,8 +5,12 @@ uniform sampler2D u_foreground;
 
 varying vec2 v_position;
 
+float darken(const float c) {
+    return max(0.0, c - 0.01);
+}
+
 void main() {
-    vec4 c1 = texture2D(u_background, 1.0 - v_position);
-    vec4 c2 = texture2D(u_foreground, 1.0 - v_position);
-    gl_FragColor = length(c2.rgb) > 0.0 ? c2 : vec4(c1.rgb * 0.97, 1);
+    vec4 b = texture2D(u_background, 1.0 - v_position);
+    vec4 c = texture2D(u_foreground, 1.0 - v_position);
+    gl_FragColor = length(c.rgb) > 0.0 ? c : vec4(darken(b.r), darken(b.g), darken(b.b), 1);
 }
