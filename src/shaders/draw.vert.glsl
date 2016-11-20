a60ec9e0621f8874f343cf3e0f766cbbe900391d
Vladimir Agafonkin <agafonkin@gmail.com>
2016-11-21 01:58:19 +0200

    add a mechanism for evening out density in different speed areas

--- src/shaders/draw.vert.glsl
+++ src/shaders/draw.vert.glsl
@@ -7,15 +7,11 @@ uniform float u_particles_tex_size;
 
 varying vec2 v_particle_pos;
 
-float decode(const vec2 channels) {
-    return dot(channels, vec2(255.0, 255.0 * 255.0)) / (255.0 * 255.0);
-}
-
 void main() {
     float x = mod(a_index, u_particles_tex_size);
     float y = floor(a_index / u_particles_tex_size);
-    vec4 particle_sample = texture2D(u_particles, vec2(x, y) / u_particles_tex_size);
-    v_particle_pos = vec2(decode(particle_sample.rg), decode(particle_sample.ba));
+    vec4 color = texture2D(u_particles, vec2(x, y) / u_particles_tex_size);
+    v_particle_pos = vec2(color.r / 255.0 + color.g, color.b / 255.0 + color.a);
 
     gl_PointSize = 1.0;
     gl_Position = vec4(1.0 - 2.0 * vec2(1.0 - v_particle_pos.x, v_particle_pos.y), 0, 1);
--- src/shaders/update.frag.glsl
+++ src/shaders/update.frag.glsl
@@ -11,10 +11,7 @@ uniform float u_rand_seed;
 varying vec2 v_position;
 
 vec2 encode(const float value) {
-    return vec2(mod(value * 255.0 * 255.0, 255.0), floor(value * 255.0)) / 255.0;
-}
-float decode(const vec2 channels) {
-    return dot(channels, vec2(255.0, 255.0 * 255.0)) / (255.0 * 255.0);
+    return vec2(mod(value * 65025.0, 255.0), floor(value * 255.0)) / 255.0;
 }
 float rand(const vec2 co) {
     float t = 12.9898 * co.x + 78.233 * co.y;
@@ -22,7 +19,7 @@ float rand(const vec2 co) {
 }
 
 vec2 lookup_wind(const vec2 uv) {
-    // manual bilinear filtering below for smoothness
+    // return texture2D(u_wind, uv * u_wind_tex_scale / u_wind_tex_size).rg; // hardware filtering
     float px = 1.0 / u_wind_tex_size;
     vec2 vc = (floor(uv * u_wind_tex_scale)) * px;
     vec2 f = fract(uv * u_wind_tex_scale);
@@ -30,25 +27,25 @@ vec2 lookup_wind(const vec2 uv) {
     vec2 tr = texture2D(u_wind, vc + vec2(px, 0)).rg;
     vec2 bl = texture2D(u_wind, vc + vec2(0, px)).rg;
     vec2 br = texture2D(u_wind, vc + vec2(px, px)).rg;
-    // return texture2D(u_wind, uv * u_wind_tex_scale / u_wind_tex_size).rg; // hardware filtering
-    return mix(mix(tl, tr, f.x), mix(bl, br, f.x), f.y);
+    return mix(mix(tl, tr, f.x), mix(bl, br, f.x), f.y); // manual bilinear filtering for smoothness
 }
 
 const float PI = 3.1415926536;
 
 void main() {
-    vec4 particle_sample = texture2D(u_particles, v_position);
-    vec2 particle_pos = vec2(decode(particle_sample.rg), decode(particle_sample.ba));
+    vec4 color = texture2D(u_particles, v_position);
+    vec2 particle_pos = vec2(color.r / 255.0 + color.g, color.b / 255.0 + color.a);
+
+    vec2 velocity = u_wind_min + (u_wind_max - u_wind_min) * lookup_wind(particle_pos);
+    float dx = velocity.x / max(0.1, cos(PI * (particle_pos.y - 0.5)));
+    float dy = -velocity.y;
+    float speed_t = length(velocity) / length(u_wind_max);
 
     vec2 seed = (particle_pos + v_position) * u_rand_seed;
-    if (rand(seed) < 0.997) {
-        vec2 velocity = u_wind_min + (u_wind_max - u_wind_min) * lookup_wind(particle_pos);
-        float dx = velocity.x / max(0.1, cos(PI * (particle_pos.y - 0.5)));
-        float dy = -velocity.y;
-        particle_pos = mod(1.0 + particle_pos + vec2(dx, dy) * 0.00002, 1.0);
-    } else {
-        particle_pos = vec2(rand(seed + 1.3), rand(seed + 2.1));
-    }
+
+    particle_pos = rand(seed) < 0.997 - speed_t * 0.01
+        ? mod(1.0 + particle_pos + vec2(dx, dy) * 0.00002, 1.0)
+        : vec2(rand(seed + 1.3), rand(seed + 2.1));
 
     gl_FragColor = vec4(encode(particle_pos.x), encode(particle_pos.y));
 }
